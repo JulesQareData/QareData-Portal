ea39a116aeb0ca6bc4b0c91bacf38ffcbeda9d83
JulesssL <julesleroux15@gmail.com>
2025-06-19 08:43:42 +0200

    V1.1

--- src/App.jsx
+++ src/App.jsx
@@ -15,17 +15,17 @@ import logo from '../public/img/Logo.png';
 function App() {
   // Données originales (non filtrées)
   const [originalQrCodes, setOriginalQrCodes] = useState(null);
-  
+
   // Données affichées (peuvent être filtrées)
   const [qrCodes, setQrCodes] = useState([
     { code: "X0M9J90", type: 'unknown', nom: "", imageUrl: "https://img.freepik.com/photos-premium/gros-point-interrogation-theme-sombre-themes-interrogatifs_183270-416.jpg" },
-    { code: "65SD6H9", type: 'Toiture', nom: "Bat B", imageUrl: "https://ars-toiture.fr/wp-content/uploads/2024/07/toiture-en-tuile.jpg"},
+    { code: "65SD6H9", type: 'Toiture', nom: "Bat B", imageUrl: "https://ars-toiture.fr/wp-content/uploads/2024/07/toiture-en-tuile.jpg" },
     { code: "42ZF85Q", type: 'Ascenseur', nom: "", imageUrl: "https://www.bonjoursenior.fr/wp-content/uploads/2019/07/Ascenseur.jpg" },
     { code: "OTCZJ4E", type: 'Porte Automatique', nom: "", imageUrl: "https://www.leasydoor.com/wp-content/uploads/2024/02/porte-automatique-1.jpg" },
     { code: "600J5GB", type: 'VMC', nom: "Vmc toiture bat B", imageUrl: "https://particulier.hellio.com/hubfs/Blog%20Particuliers%20-%20Images/remplacement-bouche-ventilation-salle-bain.jpeg" },
-    { code: "A0EDGV5", type: "Ascenseur", nom: "Bat B", imageUrl: "https://www.bonjoursenior.fr/wp-content/uploads/2019/07/Ascenseur.jpg"},
-    { code: "TA8DGG0", type: "Extincteur", nom: "", imageUrl: "https://www.datocms-assets.com/95019/1715940318-types-extincteurs-blog-spi-1.jpg?auto=compress%2Cformat"},
-    { code: "OWSBRV3", type: "Espaces verts", nom: "Espaces verts", imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSop-YNhldgbpBMjgEFucEdECrEKl_s5MJCWg&s"},
+    { code: "A0EDGV5", type: "Ascenseur", nom: "Bat B", imageUrl: "https://www.bonjoursenior.fr/wp-content/uploads/2019/07/Ascenseur.jpg" },
+    { code: "TA8DGG0", type: "Extincteur", nom: "", imageUrl: "https://www.datocms-assets.com/95019/1715940318-types-extincteurs-blog-spi-1.jpg?auto=compress%2Cformat" },
+    { code: "OWSBRV3", type: "Espaces verts", nom: "Espaces verts", imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSop-YNhldgbpBMjgEFucEdECrEKl_s5MJCWg&s" },
   ]);
 
   const [selectedType, setSelectedType] = useState(null);
@@ -54,15 +54,14 @@ function App() {
   return (
     <section id="body">
       <img src={logo} alt="Logo de QareData" className="logo" />
-      <Search 
-        qrCodes={qrCodes} 
+      <Search
+        qrCodes={qrCodes}
         setQrCodes={setQrCodes}
         originalQrCodes={originalQrCodes}
         setOriginalQrCodes={setOriginalQrCodes}
         setIsSearching={setIsSearching}
       />
-      <Title title="TERRASSES DU CHATEAU" address="10 RUE DU CHATEAU, 59000, Lille" />
-      
+
       {!selectedType ? (
         <>
           {isSearching ? (
@@ -72,6 +71,7 @@ function App() {
             </>
           ) : (
             <>
+              <Title title="TERRASSES DU CHATEAU" address="10 RUE DU CHATEAU, 59000, Lille" />
               <h2 className="h2">QR codes triés par type :</h2>
               <Types groupedQRCodes={groupedQRCodes} onTypeClick={handleTypeClick} />
             </>
@@ -79,6 +79,7 @@ function App() {
         </>
       ) : (
         <>
+          <Title title="TERRASSES DU CHATEAU" address="10 RUE DU CHATEAU, 59000, Lille" />
           <h2 className="h2">Type : {selectedType === 'unknown' ? 'Non classé' : selectedType}</h2>
           <Codes codes={selectedCodes} onBack={handleBackClick} />
         </>
